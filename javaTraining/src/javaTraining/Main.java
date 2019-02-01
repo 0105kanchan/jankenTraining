abc5bb29a36bfd0c22920c76712ff384b017828e
Kantaro Kondo <kondo@route-zero.com>
2019-02-01 18:14:51 +0900

    1,2,3以外の数字

--- javaTraining/src/javaTraining/Main.java
+++ javaTraining/src/javaTraining/Main.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Main{
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("グーなら１、チョキなら２、パーなら３を入力してください。");
 		System.out.println("Aの番です。");
 		int a = scanner.nextInt();
 		System.out.println("Bの番です。");
 		int b = scanner.nextInt();
-		
-		if((a==1 && b==2) || (a==2 && b==3) || (a==3 && b==1) ){ 
+
+		if((a==1 && b==2) || (a==2 && b==3) || (a==3 && b==1) ){
 			System.out.println("Aの勝利!");
 		}else if((a==1 && b==3) || (a==2 && b==1) || (a==3 && b==2)){
 			System.out.println("Bの勝利!");
@@ -20,6 +20,6 @@ public class Main{
 			System.out.println("あいこ!");
 		}else{
 			System.out.println("1,2,3のどれかを入力してください。");
-		}
+		 }
 	}
 }
